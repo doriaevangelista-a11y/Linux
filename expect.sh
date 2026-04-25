be18fd6004ce758e2c45c622999f2072f54f2512
doriaevangelista-a11y <doria.evangelista@gmail.com>
2026-04-25 15:33:15 -0300

    Add files via upload

---
+++ expect.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+# In the Username & Password fields, put your tacacs credentials
+# The script will log in with your username and password to make
+# the changes described in the script.
+# You must specify the file where the hosts are listed and another file with the commands, in front of the script.
+# Example: script.sh hosts.txt commands.txt
+
+# START
+
+# Declaring variables
+USERNAME=user
+PASSWORD=!@#@!#
+HOST="$1"
+CMD="$(cat $2)"
+
+for var in $(cat $1); do
+
+# Using the declared variables above to authenticate on the hosts
+
+expect -c "
+spawn telnet $var
+expect \"*Username:*\"
+send \"$USERNAME\r\"
+expect \"*Password:*\"
+send \"$PASSWORD\r\"
+expect \"*#*\"
+sleep 5
+send \"$CMD\r\"
+expect \"*#*\"
+send \"end\r\"
+expect \"*#*\"
+send \"exit\r\"
+interact
+"
+done
+
+# END
+
+#Save and exit the file.
+
+#Apply execution permission:
+
+#chmod 755 file.sh
+#To execute:
+
+bash
+./file.sh hosts-file commands-file
